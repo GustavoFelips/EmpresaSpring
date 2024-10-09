6da593df4987adac18409c8409b5dc4df7f6622a
GustavoFelips <130930887+GustavoFelips@users.noreply.github.com>
2024-10-09 13:54:17 +0000

    update

--- Curriculo/src/main/java/com/example/demo/service/EmpresaService.java
+++ Curriculo/src/main/java/com/example/demo/service/EmpresaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.Empresa;
 import com.example.demo.repository.EmpresaRepository;
